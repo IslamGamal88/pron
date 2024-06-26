f0d4a8a4c9fc4b443b115b3efc7b8f47fb162edb
Islam Abdelkader <islam.abdelkader@lendinvest.com>
2024-06-26 14:44:43 +0300

    remove unwanted

--- main.js
+++ main.js
@@ -2,38 +2,38 @@ const http = require("http");
 const path = require("path");
 const express = require("express");
 const socketio = require("socket.io");
-const parseArgs = require("minimist");
-const fs = require("fs");
+// const parseArgs = require("minimist");
+// const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const args = parseArgs(process.argv.slice(2));
-const { name = "default", port = "8080" } = args;
+// const args = parseArgs(process.argv.slice(2));
+// const { name = "default", port = "8080" } = args;
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/api/test", (req, res) => {
-  res.json({
-    headers: req.headers,
-    address: req.connection.remoteAddress,
-  });
-});
+// app.get("/api/test", (req, res) => {
+//   res.json({
+//     headers: req.headers,
+//     address: req.connection.remoteAddress,
+//   });
+// });
 
-app.get("/api/name", (req, res) => {
-  res.json({ name });
-});
+// app.get("/api/name", (req, res) => {
+//   res.json({ name });
+// });
 
-app.get("/api/info", (req, res) => {
-  fs.readFile(`${__dirname}/version.txt`, "utf8", (err, version) => {
-    res.json({
-      version: version || 0,
-      dirname: __dirname,
-      cwd: process.cwd(),
-    });
-  });
-});
+// app.get("/api/info", (req, res) => {
+//   fs.readFile(`${__dirname}/version.txt`, "utf8", (err, version) => {
+//     res.json({
+//       version: version || 0,
+//       dirname: __dirname,
+//       cwd: process.cwd(),
+//     });
+//   });
+// });
 
 io.on("connection", (sock) => {
   console.log("Client connected");
